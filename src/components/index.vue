1d46376e0584639f911c7b58e6ff4d98d7697e2d
user <icupayliedie@gmail.com>
2021-05-19 23:38:48 +0300

    Начало положено, пока что есть bar и текст

---
+++ src/components/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+
+      <v-col class="mb-4">
+        <p class="subheading font-weight-regular">
+          Здесь будет список сотрудников и форма ввода данных
+          <br>Нужно разделить на две части
+
+        </p>
+      </v-col>
+
+
+
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+  }
+</script>
